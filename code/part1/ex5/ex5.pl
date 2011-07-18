19797057ebd1bf79f3578908df6cd0252748abb1
Adam Taylor <adam.taylor@net-a-porter.com>
2011-07-18 16:10:35 +0100

    Add rest of exercise folders

---
+++ code/part1/ex5/ex5.pl
@@ -0,0 +1,27 @@
+#!/opt/xt/xt-perl/bin/perl
+use Dancer;
+use DateTime;
+
+set logger => 'console';
+set log => 'debug';
+set show_errors => 1;
+
+get '/', sub {
+    return "<h1>Hello World!</h1>";
+};
+
+get '/hello' => sub {
+    return "<h1>Hello Things!</h1>";
+};
+
+get '/tt-time' => sub {
+    my $now = DateTime->now();
+
+    template 'time' => { time => '11:24:09', date => '15 Jul 2011' };
+};
+
+get '/hello-adj-par/:adjective' => sub {
+    return "<h1>Hello ".params->{adjective} ." Things!</h1>";
+};
+
+Dancer->dance;
